4e8f64892d41c8e00714fcf8fc5bd73a6418bed2
Li.DingBin <lidingbing@laoken.com>
2024-10-16 16:56:26 +0800

    'modify'

---
+++ Sixnet.Web/Serialization/Json/Converter/WebFileFullPathJsonConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Sixnet.Web.Utility;
+
+namespace Sixnet.Web.Serialization.Json.Converter
+{
+    public sealed class WebFileFullPathJsonConverter : JsonConverter<string>
+    {
+        readonly string fileObjectName;
+
+        private WebFileFullPathJsonConverter(string fileObjectName)
+        {
+            this.fileObjectName = fileObjectName;
+        }
+
+        public override string Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return reader.GetString();
+        }
+
+        public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(HttpClientHelper.GetFullPath(value, fileObjectName));
+        }
+
+        public static WebFileFullPathJsonConverter GetInstance(string fileObjectName)
+        {
+            return new WebFileFullPathJsonConverter(fileObjectName);
+        }
+    }
+}
---
+++ Sixnet.Web/Serialization/Json/WebFileFullPathAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Text.Json.Serialization;
+using Sixnet.Web.Serialization.Json.Converter;
+
+namespace Sixnet.Web.Serialization.Json
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    public class WebFileFullPathAttribute : JsonConverterAttribute
+    {
+        static readonly ConcurrentDictionary<string, WebFileFullPathJsonConverter> _converters = new();
+
+        public string FileObjectName { get; set; }
+
+        public WebFileFullPathAttribute(string fileObjectName = "")
+        {
+            FileObjectName = fileObjectName;
+        }
+
+        public override JsonConverter CreateConverter(Type typeToConvert)
+        {
+            return _converters.GetOrAdd(FileObjectName, k => WebFileFullPathJsonConverter.GetInstance(k));
+        }
+    }
+}
